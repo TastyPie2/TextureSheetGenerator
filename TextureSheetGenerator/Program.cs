89cfa503193080c4c7ac7156d3e8e2e168d585a6
Mathias Hansen <mathias04.hansen@outlook.com>
2022-01-29 16:30:06 +0100

    Add project files.

---
+++ TextureSheetGenerator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+using TextureSplicer;
+
+class Program
+{
+    static readonly string inputPath = Path.Combine(Environment.CurrentDirectory, "Input");
+    static readonly string outputPath = Path.Combine(Environment.CurrentDirectory, "Output");
+    static void Main()
+    {
+        if (!Directory.Exists(inputPath))
+        {
+            Console.WriteLine("InputFolder is missing generating...");
+            Directory.CreateDirectory(inputPath);
+            Thread.Sleep(1000);
+            Environment.Exit(0);
+        }
+        if (!Directory.Exists(outputPath))
+        {
+            Directory.CreateDirectory(outputPath);
+        }
+
+        string[] files = Directory.GetFiles(inputPath);
+        
+        if(files.Length <= 1)
+        {
+            Console.WriteLine("Not enougth images.");
+            Thread.Sleep(1000);
+            Environment.Exit(0);
+        }
+
+
+        Console.WriteLine("Assebling texture sheet...");
+        Bitmap bitmap = Splicer.SpliceTextures(files);
+        Console.WriteLine("Done\nSaving sheet...");
+        string outputLocation = outputPath + "\\" + $"{DateTime.Now.TimeOfDay.Seconds}.{DateTime.Today.Minute}.{DateTime.Now.Hour}.{DateTime.Now.Day}.{DateTime.Now.Month}.{DateTime.Now.Year}" + ".png";
+        bitmap.Save(outputLocation);
+        Console.WriteLine("Saved sheet at \"{0}\"", outputLocation);
+        Console.WriteLine("Done.");
+        Console.WriteLine("Press enter to close the application...");
+        Console.Read();
+    }
+}
---
+++ TextureSplicer/Splicer.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Numerics;
+
+namespace TextureSplicer
+{
+    public class Splicer
+    {
+        public static Bitmap SpliceTextures(string[] filePaths)
+        {
+#pragma warning disable CA1416
+            Bitmap spriteSheet;
+#pragma warning disable CS8619
+            List<Lazy<Bitmap>?> bitmaps = LoadLazyBitmaps(filePaths);
+#pragma warning restore CS8619
+
+            Vector2 heightAndWidth = new Vector2(bitmaps[0].Value.Width, bitmaps[0].Value.Height);
+
+            int sqrt = (int)Math.Ceiling(Math.Sqrt(bitmaps.Count));
+            spriteSheet = new Bitmap((int)(sqrt * heightAndWidth.X), (int)(sqrt * heightAndWidth.Y));
+            Console.WriteLine("Atlas size {0}x{1}", spriteSheet.Height, spriteSheet.Width);
+
+            Vector2 offset = new Vector2(0, spriteSheet.Height - heightAndWidth.Y);
+            for (int i = 0; i < bitmaps.Count; i++)
+            {
+                Console.WriteLine("Bitmap {0}/{1}", i, bitmaps.Count);
+                Console.WriteLine("Offset: {0}X {1}Y", offset.X, offset.Y);
+                Lazy<Bitmap>? lazyBitmap = bitmaps[i];
+                
+                if (lazyBitmap == null)
+                    continue;
+
+                if (offset.X >= spriteSheet.Width)
+                {
+                    offset.Y = Math.Clamp(offset.Y - heightAndWidth.Y, 0, spriteSheet.Height - heightAndWidth.Y);
+                    offset.X = 0;
+                }
+
+                Bitmap bitmap = lazyBitmap.Value;
+                Dictionary<Vector2, Color> pixels = GetAllPixels(bitmap);
+
+                foreach (KeyValuePair<Vector2, Color> pixel in pixels)
+                {
+                    spriteSheet.SetPixel((int)(pixel.Key.X + offset.X), (int)(pixel.Key.Y + offset.Y), pixel.Value);
+                }
+
+                offset.X += heightAndWidth.X;
+            }
+
+            return spriteSheet;
+        }
+
+        private static Dictionary<Vector2, Color> GetAllPixels(Bitmap bitmap)
+        {
+            Dictionary<Vector2, Color> pixels = new();
+            for (int y = 0; y < bitmap.Height; y++)
+                for (int x = 0; x < bitmap.Width; x++)
+                    pixels.Add(new Vector2(x, y), bitmap.GetPixel(x, y));
+
+            return pixels;
+        }
+
+        private static List<Lazy<Bitmap>> LoadLazyBitmaps(string[] filePaths)
+        {
+            List<Lazy<Bitmap>> bitmaps = new();
+
+            foreach (string i in filePaths)
+                bitmaps.Add(new Lazy<Bitmap>(new Bitmap(i)));
+            return bitmaps;
+        }
+
+        private static Vector2 GetTotalHeightAndWidth(List<Lazy<Bitmap>?> bitmaps)
+        {
+            int totalWidth = 0;
+            int totalHeight = 0;
+
+            bitmaps.ForEach(bmp =>
+            {
+                if (bmp != null)
+                {
+                    Bitmap bitmap = bmp.Value;
+
+                    totalWidth += bitmap.Width;
+                    totalHeight += bitmap.Height;
+
+                    bmp = null;
+                }
+            });
+
+            return new Vector2(totalWidth, totalHeight);
+        }
+    }
+#pragma warning restore CA1416
+}
